b3b120c1400835a56c3a08316336c2d3048ca91c
tarabite <88362366+tarabite@users.noreply.github.com>
2021-12-25 22:34:05 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by Tara Diego and{" "}
-        <a href="https://github.com/tarabite/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          Coded by Tara Diego and{" "}
+          <a
+            href="https://github.com/tarabite/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
