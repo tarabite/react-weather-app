c472629a3109916b9494f5313afd9e1aaa84e995
tarabite <88362366+tarabite@users.noreply.github.com>
2021-12-25 22:12:17 -0800

    Added Git repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by Tara Diego and{" "}
+        <a href="https://github.com/tarabite/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
