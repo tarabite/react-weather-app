2adf860f13e2dd1eee2e886a129c473cffb8bc07
tarabite <88362366+tarabite@users.noreply.github.com>
2021-12-26 20:33:49 -0800

    added FormattedDate component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -12,7 +13,7 @@ export default function Weather(props) {
       city: response.data.name,
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.dt * 1000),
       iconUrl: `https://ssl.gstatic.com/onebox/weather/64/rain.png`,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
@@ -45,7 +46,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
