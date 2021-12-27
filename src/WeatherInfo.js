2a5ee837e2f0996299c973426352cdb9aacf865d
tarabite <88362366+tarabite@users.noreply.github.com>
2021-12-26 22:58:41 -0800

    created WeatherTemperature component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,21 +14,20 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
+        <div className="col-8">
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} />
             </div>
 
-            <div className="temperature float-left">
-              {Math.round(props.data.temperature)}
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
-            <div className="unit">°C</div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-4">
           <ul>
-            <li>Humidity: {props.data.humidity}</li>
+            <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
             <li>
               H: {Math.round(props.data.max)}°C | L:{" "}
